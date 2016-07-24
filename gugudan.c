9da70b62f9d4d81f48f87b95c2c49958dff91fa8
jongpak <jplabs@jongpak.com>
2016-07-24 12:46:19 +0900

    불필요한 for문 없애고 최적화시킴

--- gugudan.c
+++ gugudan.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
+// 사용자로부터 입력받는 수가 저장될 변수 a만 있어도 충분함.
+// for 문은 조건이 되면 자동으로 탈출하기 때문에 break 문이 없어도 됨.
+
 int main() {
-	int a, b;
+	int a;
 
 	scanf("%d", &a);
 
-	for (int i = a; i <= 9; i++) {
-		for (int j = 1; j <= 9; j++) {
-			b = i * j;
-			printf("%d * %d = %d\n", i, j, b);
-		}
-		break;
+	for (int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", a, i, a * i);
 	}
 
 	return 0;
