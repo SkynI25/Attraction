c5f76dad169060366bffdc261186293c354bdd65
jongpak <jplabs@jongpak.com>
2016-07-24 12:48:16 +0900

    동적할당 없애고 for문을 하나로 줄임 - 동적할당 사용한 거 잘했고! 동적할당 연습 분명히 필요해~ - 다만 효율성에 맞게 동적할당과 for문을 없애보았어

--- smallerX.c
+++ smallerX.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+// 잘 생각해보면 동적할당(배열)없이도 구현할 수 있고, for문도 하나로 해결할 수 있음.
+// 기존 구현이 나쁘다는 뜻이 아님! 동적할당으로 잘 구현했음 great!
+
+// 기존에 동적할당은 없앰으로써 --> 메모리 효율성을 높혔고,
+// for문을 하나로 줄임으로써 --> 2n의 시간복잡도를 n으로 줄임.
 
 int main() {
 	int N, x, num;
-	int *parr;
 
 	scanf("%d %d", &N, &x);
-
-	parr = (int *)malloc(sizeof(int) * N);
 
 	for (int i = 0; i < N; i++) {
 		scanf("%d", &num);
 
-		parr[i] = num;
+		if (num < x)
+			printf("%d ", num);
 	}
 
-	for (int i = 0; i < N; i++) {
-		if (parr[i] < x)
-			printf("%d ", parr[i]);
-	}
 	return 0;
 }
