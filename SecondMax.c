ec22ee2416458f5ce00f21fb90b9499568feca14
jongpak <jplabs@jongpak.com>
2016-07-24 12:04:35 +0900

    if문 사용법에 대한 문제의 취지에 맞게 if문만을 이용하도록 바꿈 - 이 문제에서는 3개의 수를 비교하는 것이기 때문에 비용이 큰 for문이나 정렬 알고리즘 까지는 필요없음.

--- SecondMax.c
+++ SecondMax.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
 
+// 이 문제는 if문 사용법에 대한 문제다.
+// 숫자 여러 개중에 2번째로 큰 수를 고르는 거라면,
+// 반복문이나 정렬 알고리즘을 생각해볼 수 있는데
+// 고작 3개의 수 중에서 2번째로 큰 수를 고르는 것이므로
+// 조건문 몇개로 간단히 판별이 가능하다.
+
 int main() {
-	int num1, num2, num3, max1 = 0, max2 = 0, tmp;
+	int tmp = 0;
 	int arr[3];
 
-	scanf("%d %d %d", &num1, &num2, &num3);
+	scanf("%d %d %d", &arr[0], &arr[1], &arr[2]);
 
-	arr[0] = num1;
-	arr[1] = num2;
-	arr[2] = num3;
+	if ((arr[0] >= arr[1] && arr[1] >= arr[2] && arr[0] >= arr[2])
+		|| (arr[0] <= arr[1] && arr[1] <= arr[2] && arr[0] <= arr[2]))
+		tmp = 1;
+	else if ((arr[0] <= arr[1] && arr[1] >= arr[2] && arr[0] <= arr[2])
+		|| (arr[0] >= arr[1] && arr[1] <= arr[2] && arr[0] >= arr[2]))
+		tmp = 2;
+	else
+		tmp = 0;
 
-	for (int i = 0; i < 3; i++) {
-		if (arr[0] > arr[1]) {
-			tmp = arr[0];
-			arr[0] = arr[1];
-			arr[1] = tmp;
-			if (arr[1] > arr[2]) {
-				tmp = arr[1];
-				arr[1] = arr[2];
-				arr[2] = tmp;
-			}
-		}
-		else if (arr[0] > arr[2]) {
-			tmp = arr[0];
-			arr[0] = arr[2];
-			arr[2] = tmp;
-			if (arr[1] > arr[2]) {
-				tmp = arr[1];
-				arr[1] = arr[2];
-				arr[2] = tmp;
-			}
-		}
-	}
-	for (int i = 0; i<3; i++) {
-		if (arr[i] >= max1) {
-			max2 = max1;
-			max1 = arr[i];
-		}
-		else if ((arr[i] > max2 && arr[i] < max1) || max1 == max2) {
-			max2 = arr[i];
-		}
-	}
-	printf("%d", max2);
+	printf("%d", arr[tmp]);
 	return 0;
 }
