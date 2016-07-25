95fa8874a24ef83c0600d49321d28ff2c543ebc2
jongpak <jplabs@canxan.com>
2016-07-25 23:25:30 +0900

    룩업테이블을 이용하고, 불필요한 계산과 공간 낭비를 최적화함 - 월의 날짜수와 요일을 룩업테이블화 시킴. - 기존의 불필요한 공간낭비(arr 변수)를 없애고 필요한 계산만 함.

--- Calander.c
+++ Calander.c
@@ -1,72 +1,55 @@
 #include <stdio.h>
 
+// 기존의 코드에서 단축시킬 수 있는 여지가 굉장히 많아.
+// 일단 arr[12][365] 이렇게 (12 * 365)개의 원소를 만들었는데, 낭비되는 공간이 많아.
+// arr[12][31] 이게 더 적절한 적절할 것 같아. 최대 (12 * 31) 개만 사용하기 때문이야.
+//
+// 그리고 기존 코드에서 1월부터 12월까지 '모든' 요일을 arr[12][365]에다가 미리 구하는 데,
+// 이 문제에서는 전체를 구하기 보다는 필요한 것만 구하는 것이 효율적일 것 같아.
+//
+// 또한 월에 해당하는 수를 더할 때, 아래의 daysOfMonth 처럼
+// 룩업(lookup)테이블을 이용하면 훨씬 편하게 사용할 수 있어.
+//
+// 마찬가지로 요일을 기존에는 switch..case 문을 이용하였는데, 아래의 week 처럼 룩업테이블을 이용하면
+// 코드 가독성도 좋아질 뿐더러 유지보수에도 좋아~
+
 int main() {
 
-	int arr[12][365];
+	int month, day, index = 0, i = 0;
 
-	int month, day, index=0, chgindex=0, i=0, j=0;
+	int daysOfMonth[12 + 1] = {
+		0,			// month 인덱스 맞추기 위해 넣은 더미값
+		31,			// 1월
+		28,			// 2월
+		31,			// 3월
+		30,			// 4월
+		31,			// 5월
+		30,			// 6월
+		31,			// 7월
+		31,			// 8월
+		30,			// 9월
+		31,			// 10월
+		30,			// 11월
+		31			// 12월
+	};
+	char* week[] = {
+		"MON",		// 0
+		"TUE",		// 1
+		"WED",		// 2
+		"THU",		// 3
+		"FRI",		// 4
+		"SAT",		// 5
+		"SUN"		// 6
+	};
 
-	for (i = 0; i < 12; i++) {
-		if (i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) {
-			for (j = 0; j < 31; j++) {
-				if (j != 30) {
-					arr[i][j] = index % 7;
-					index++;
-				}
-				else if (j == 30 && i == 0) {
-					arr[i][j] = index % 7;
-					index = (chgindex + 3) % 7;
-				}
-				else {
-					arr[i][j] = index % 7;
-					index++;
-					//chgindex += 3;
-					//index = (chgindex + 3) % 7;
-				}
-			}
-		}
-		else if (i == 3 || i == 5 || i == 8 || i == 10) {
-			for (j = 0; j < 30; j++) {
-				if (j != 29) {
-					arr[i][j] = index % 7;
-					index++;
-				}
-				else {
-					arr[i][j] = index % 7;
-					index++;
-					//index = (chgindex + 2) % 7;
-				}
-			}
-		}
-		else {
-			for (j = 0; j < 28; j++) {
-					arr[i][j] = index % 7;
-					index++;
-					if (j == 27) {
-						index = 3;
-					}
-			}
-		}
+	scanf_s("%d %d", &month, &day);
+
+	for (i = 1; i < month; i++) {
+		index += daysOfmonth[i];
 	}
+	index += day - 1;
 
-	for (int n = 0; n < 100; n++) {
+	printf("%s", week[index % 7]);
 
-		scanf_s("%d %d", &month, &day);
-
-		month -= 1;
-		day -= 1;
-
-		switch (arr[month][day] % 7) {
-		case 0: printf("MON\n"); break;
-		case 1: printf("TUE\n"); break;
-		case 2: printf("WED\n"); break;
-		case 3: printf("THU\n"); break;
-		case 4: printf("FRI\n"); break;
-		case 5: printf("SAT\n"); break;
-		case 6: printf("SUN\n"); break;
-		}
-
-	}
-	getchar();
 	return 0;
 }
