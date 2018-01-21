b385407e540b3f80bf9c626816a5ab55ab033a3e
Park Ki Tae <rlxo1651@daum.net>
2018-01-21 23:26:13 +0900

    1차원 배열 사용하기 - 숫자의개수 (문제번호 : 2577) 세 개의 자연수 A, B, C가 주어질 때 A×B×C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오. [URI] https://www.acmicpc.net/problem/2577

---
+++ p2577.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <string>
+using namespace std;
+int main() {
+
+	int arr[10] = { 0 };
+	int a, b, c,ans;
+
+	scanf("%d %d %d", &a,&b,&c);
+
+	ans = a * b * c;
+
+	while (ans != 0) {
+		arr[ans % 10] += 1;
+		ans /= 10;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << endl;
+	}
+
+	return 0;
+
+}
